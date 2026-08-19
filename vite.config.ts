80df0c9fcb059df3b56d509d4f870e8c5d8391a3
Alexandre Linhares <alexandrelinharesxd@gmail.com>
2026-08-19 11:51:13 -0300

    build: update base path to /CSP/

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/supti/',
+    base: '/CSP/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
